2395facd11e34c007da9022150069c13b61d480b
Pranav R <pranav81234@gmail.com>
2026-04-06 12:49:48 +0530

    feat: added page routes; setup themes; added assets

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,20 @@
 import "@/global.css";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+import { styled } from 'nativewind';
+const SafeAreaView = styled(RNSafeAreaView)
  
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
       <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">Go to Onboarding</Link>
       <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Go to Sign In</Link>
       <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Go to Sign Up</Link>
-      <Link href="/(tabs)/subscriptions/spotify" className="mt-4 rounded bg-primary text-white p-4">Go to Spotify</Link>
-      <Link href="/(tabs)/subscriptions/claude" className="mt-4 rounded bg-primary text-white p-4">Go to Claude</Link>
-    </View>
+    </SafeAreaView>
   );
 }
